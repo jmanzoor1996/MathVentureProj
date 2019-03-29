281ab349903506e899391c3603224ec76afdea71
jvionlife <jmanzoor@radford.edu>
2019-03-29 00:07:55 -0400

    UploadingProject
    WholeProject

---
+++ MathVenture/Assets/Scenes/MenuUIManger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIManger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Play Button
+    public void Play() {
+
+        SceneManager.LoadScene("board");
+
+
+    }
+
+    //Options Button
+    public void Options() {
+
+        SceneManager.LoadScene("options");
+
+    }
+
+    //MainMenu
+    public void GoBack() {
+
+        SceneManager.LoadScene("Menu");
+    }
+
+}
---
+++ MathVenture/Assets/Scripts/RandomGen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomGen : MonoBehaviour
+{
+
+    public GameObject TextBox;
+    public int TheNumber;
+
+    public void randomGenerator()
+    {
+        TheNumber = Random.Range(1, 7);
+
+        TextBox.GetComponent<Text> ().text = "" + TheNumber;
+
+    }
+
+  
+}
