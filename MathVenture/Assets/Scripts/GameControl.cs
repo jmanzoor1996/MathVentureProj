74de5182899ad571f56f7e1c40baeba5e82fa913
ipaterson33 <48026320+ipaterson33@users.noreply.github.com>
2019-04-01 22:00:22 -0400

    Major updates, Still working on Movement, UI pause works.

---
+++ MathVenture/Assets/Scripts/GameControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+
+    private static GameObject player1, player2;
+
+    public static int diceRoll = 0;
+    public static int playerStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player1 = GameObject.Find("spritereal");
+        playerStart = player1.GetComponent<Path>().waypoints.Length - 1;
+
+        player1.GetComponent<Path>().moveable = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player1.GetComponent<Path>().waypointIndex > playerStart - diceRoll)
+        {
+            playerStart = player1.GetComponent<Path>().waypointIndex - 1;
+        }
+
+        /*if (0 == player1.GetComponent<Path>().waypointIndex)
+        {
+         
+        }*/
+    }
+
+    public static void MovePlayer(int player)
+    {
+        switch (player)
+        {
+            case 1:
+                player1.GetComponent<Path>().moveable = true;
+                break;
+    }
+    }
+}
---
+++ MathVenture/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool isGamePaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        SceneManager.LoadScene("board2");
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    public void Pause()
+    {
+        SceneManager.LoadScene("PauseMenu");
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+}
